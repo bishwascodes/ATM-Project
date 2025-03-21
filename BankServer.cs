99b9a2da897aec4a56c5e01d0627fffc487b892d
Landon Ginn <landon.ginn@students.snow.edu>
2025-03-21 11:01:30 -0600

    All methods added BankServer v1

--- BankServer.cs
+++ BankServer.cs
@@ -4,52 +4,54 @@ namespace ATM;
 
 public class BankServer
 {
-   private Dictionary<string, (int pin, BankAccount account)> validCards = new();
+    private Dictionary<string, (int pin, BankAccount account)> validCards = new();
 
-   public BankServer(Dictionary<string, (int pin, BankAccount account)> initialCards)
-   {
-    validCards = initialCards;
-   }
-
-public bool verifyCard(string cardNumber)
-{
-    if (!validCards.ContainsKey(cardNumber))
+    public BankServer(Dictionary<string, (int pin, BankAccount account)> initialCards)
     {
-        return false;
+        validCards = initialCards;
     }
-    else 
-    {
-        return true;
-    }
-}
 
-public bool verifyPIN(string cardNumber, int pin)
-{
-    if (verifyCard(cardNumber))
+    public bool verifyCard(string cardNumber)
     {
-        if (validCards[cardNumber].pin == pin){
+        if (!validCards.ContainsKey(cardNumber))
+        {
+            return false;
+        }
+        else
+        {
             return true;
         }
     }
-    return false; 
-}
 
-public bool processTransaction(string cardNumber, double amount)
-{
-    if (validCards[cardNumber].account.hasSufficientFunds(amount))
+    public bool verifyPIN(string cardNumber, int pin)
     {
-        
+        if (verifyCard(cardNumber))
+        {
+            if (validCards[cardNumber].pin == pin)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public bool processTransaction(string cardNumber, double amount)
+    {
+        if (validCards[cardNumber].account.hasSufficientFunds(amount))
+        {
+            bool transactionSuccessful = validCards[cardNumber].account.Withdraw(amount);
+            return transactionSuccessful;
+        }
+        return false;
+    }
+
+    public double checkBalance(string cardNumber)
+    {
+        if (validCards.ContainsKey(cardNumber))
+        {
+            return validCards[cardNumber].account.getBalance();
+        }
+        return -1;
     }
 }
 
-public double checkBalance(string cardNumber)
-{
-    throw new NotImplementedException();
-    int x = 0;
-}
-
-}
-
-
-
-public class BankAccount();
